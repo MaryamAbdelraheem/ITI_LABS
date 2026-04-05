568383c28d739b785b386a0e35696eb0b7aca87a
Maryam Abdelraheem <maryamabdelraheem86@gmail.com>
2026-04-05 11:04:54 +0200

    py_update

--- python/lab1/lab1-2.py
+++ python/lab1/lab1-2.py
@@ -1,38 +1,24 @@
-#write a program that build a mario pyramid like below:
-#without loop: 
+#0. pyramid:
+number = int(input("Enter the number: "))
 
-num = int(input('enter the levels of the pyramid: '))
-
-print("*" * 1)
-print("*" * 2)
-print("*" * 3)
-print("*" * num)
-
-
-
-
-
-#write a program that prints the locations of 'i' character in any string ypu added
-
-#without loop:
+for i in range(number):
+    print(" " * (number - i - 1) + "*" * (i+1))
+    
+# 1. Locations of 'i' in a string
 string = input("Enter a string: ")
+print("--------------------------------------------------")
 
-loc1 = string.find('i')
-loc2 = string.find('i', loc1 + 1)
-loc3 = string.find('i', loc2 + 1)
-loc4 = string.find('i', loc3 + 1)
-loc5 = string.find('i', loc4 + 1)
-
-print("The character 'i' is found at indexes: {} {} {} {} {}".format(loc1, loc2, loc3, loc4, loc5))
-
-#with loop
-string = input("Enter a string: ")
-
-locations = []
-loc = string.find('i')
-
-while loc != -1:
-    locations.append(loc)
-    loc = string.find('i', loc + 1)
+locations = []                         #lowerr
+for i in range(len(string)):
+    if string[i] == 'i':
+        locations.append(i)
 
 print("The character 'i' is found at indexes: {}".format(locations))
+
+print("--------------------------------------------------")
+
+# 2. Multiplication table      checkkk
+number = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print("{} x {} = {}".format(number, i, number * i))
